150f9fa1aac15ca10b2dff16f6f4cf5003bb7c28
Rusiru Erandaka <erandak-ec20037@stu.kln.ac.lk>
2025-04-21 22:22:17 +0530

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import express, { Request, Response } from 'express';
+import { Routes } from './routes';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello, Rusiru Erandaka');
+});
+
+app.use('/api', new Routes().router);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
